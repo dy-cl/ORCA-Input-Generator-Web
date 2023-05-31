fb28b68dc092ccab5262b63d023286558f5ff563
dy-cl <dylanrcounsell@gmail.com>
2023-05-31 13:16:02 +0100

    Fixed InChi inputs.

--- oig_app/get_xyz.py
+++ oig_app/get_xyz.py
@@ -8,30 +8,40 @@ def to_xyz(mol_string, mol_input_format):
     Parameters:
     - molecule_string (str): Molecular input string.
     - mol_input_format (str): Initial format type of the molecular string.
+        Valid values: 'smiles', 'inchi'
 
     Returns:
-    - None: If XYZ coordinates could not be generated.
+    - None: If XYZ coordinates and InChI could not be generated.
     - xyz_content: Otherwise.
     """
-    # Create RDKit molecule object
-    mol = Chem.MolFromSmiles(mol_string)
+    if mol_input_format == 'smiles':
+        #Create RDKit molecule object from SMILES
+        mol = Chem.MolFromSmiles(mol_string)
+        if mol is None:
+            print("Error: Failed to read the molecule from SMILES.")
+            return None
 
-    if mol is None:
-        print("Error: Failed to read the molecule.")
+    elif mol_input_format == 'inchi':
+        #Create RDKit molecule object from InChI
+        mol = Chem.MolFromInchi(mol_string)
+        if mol is None:
+            print("Error: Failed to read the molecule from InChI.")
+            return None
+
+    else:
+        print("Error: Invalid input format. Valid formats are 'smiles' and 'inchi'.")
         return None
 
-    # Add implicit hydrogens
+    #Add implicit hydrogens
     mol = Chem.AddHs(mol)
 
-
-    # Generate 3D coordinates
-    mol = Chem.AddHs(mol)
+    #Generate 3D coordinates
     AllChem.EmbedMolecule(mol)
 
-    # Write to XYZ format
+    #Write to XYZ format
     xyz_content = Chem.MolToXYZBlock(mol)
 
-    # Remove the first line (atom count)
+    #Remove the first line (atom count)
     lines = xyz_content.strip().split('\n')
     xyz_content = '\n'.join(lines[1:])
 
