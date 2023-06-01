2116e0a814347a3f3746a900593d360115bb1b55
dylan <dylanrcounsell@gmail.com>
2023-06-01 20:10:11 +0100

    Added download button and general CSS changes.

--- oig_app/get_title.py
+++ oig_app/get_title.py
@@ -2,11 +2,8 @@ def make_title(mol_name, calculation_type, method_basis):
 
     calculation_type = calculation_type.replace('!', '')
 
-    if calculation_type == '':
-        calculation_type = 'ENER'
-
     method_basis = method_basis.replace('!', '')
-    title = str(mol_name) + ' ' + str(calculation_type) + str(method_basis)
+    title = str(mol_name) + str(calculation_type) + str(method_basis)
     title_string = f"#\n# {title}\n#"
 
     return title_string
